d1652997fdfaa3e29cbdfff6af0db454e2a3f5e6
Jeffrey <Jeffrey@eyee.com>
2019-04-06 18:25:45 +0800

    笔记先更新一把

--- Android部分/SYGameProxy/app/src/main/java/www/sy/com/sygameproxy/game/SYMainActivity.kt
+++ Android部分/SYGameProxy/app/src/main/java/www/sy/com/sygameproxy/game/SYMainActivity.kt
@@ -1,16 +1,15 @@
-package www.sy.com.sygameproxy.game;
+package www.sy.com.sygameproxy.game
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
 
-import www.sy.com.sygameproxy.R;
+import www.sy.com.sygameproxy.R
 
-public class SYMainActivity extends AppCompatActivity {
+class SYMainActivity : AppCompatActivity() {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_symain);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_symain)
 
         /*
         * Android是一个针对移动设备的操作系统/软件平台
@@ -84,15 +83,5 @@ public class SYMainActivity extends AppCompatActivity {
         * onStart()
         * onReStart()
         * */
-
-
-
-
-
-
-
-
-
-
     }
 }
