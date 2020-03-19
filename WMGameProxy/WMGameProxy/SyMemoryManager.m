c88d4f06094815b0cfe37749c5cb370c30e697af
Jeffrey <Jeffrey@eyee.com>
2020-03-20 00:31:36 +0800

    note上传

--- WMGameProxy/WMGameProxy/SyMemoryManager.m
+++ WMGameProxy/WMGameProxy/SyMemoryManager.m
@@ -7,9 +7,44 @@
 //
 
 #import "SyMemoryManager.h"
+#import "WMGameProxy.h"
+/**
+ 0.内存管理的重要性
+ 1>.移动设备的内存极其有限，如果不进行内存管理App很容易发生闪退
+ 2>.增加一个App的内存占用行为 - 创建一个OC对象/定义一个变量/调用一个函数和方法
+ 3>.App占用内存较多时候系统会发生内存警告（回收一些不需要的内存空间）/系统会强制关闭App
+ */
+/**
+ 1.什么是内存管理
+ 1>.如何回收那些不需要再使用的对象 - 内存管理
+ 2>.分配内存 - 比如创建一个对象/清除内存 - 销毁一个对象
+ 3>.内存管理的范围 - 任何继承于 NSobject 的对象/对非对象类型（比如基本数据类型 - int/char/double）无效
+ */
+/**
+ 2.堆和栈的区别
+ 栈 - 由编译器自动分配释放内存/主要存放函数的参数值、局部变量
+ 堆 - 由程序员手动分配释放内存/若程序员不释放，程序结束的时候才会回收
+ */
+/**
+ 3.什么是引用计数器
+ 0>.每个对象都有一个自己的引用计数器/它是一个整数/表示对象被引用的次数（多少人正在引用这个对象）
+ 1>.系统如何判断什么时候需要回收一个对象 - 对象的 "引用计数器retainCount = 0"
+ 2>.任何对象只要被创建出来“引用计数器retainCount = 1”
+ */
+/**
+ 4.怎么操作引用计数器
+ 1>.给对象发送一个retain消息->可以使对象引用计数器+1（retain方法返回对象本身）
+ 2>.给对象发送一个release消息->可以使对象引用计数器-1/release不代表销毁/回收对象（仅仅代表引用计数器-1）
+ 3>.给对象发送一个retainCount消息->可以获取当前引用计数器的值（获取的不准备/要想准备必须使用dealloc()方法）
+ */
 
 @implementation SyMemoryManager
-
+/**
+5.dealloc()方法
+1>.什么时候调用 - 当对象的 "引用计数器retainCount = 0"的时候
+2>.作用 - 销毁对象/释放相关资源/必须调用 “[super dealloc];” 放在最后
+3>.一旦对象被回收就不能在使用/如果坚持使用会导致程序崩溃（野指针错误）
+*/
 - (void)dealloc {
     /**
      执行代码
@@ -18,21 +53,68 @@
     [super dealloc];
 }
 
-#pragma mark - 内存管理
-// 管理对象的生命周期/基本数据类型不需要内存管理
-// 内存管理有几种方式？？？
-// 自动调用 “dealloc方法”
-// ARC/MRC混合编程-选中工程->TARGET->Build Settings->Automatic Reference Counting->NO
--(void)memoryManager {
-    // 对象的内存存储细节
-    // 内存分成三个区域：栈/堆/代码区
-    // 程序加载到内存中：存放在"代码区(存放二进制代码)"
-    /*
-     通过类创建对象：
-     1.开辟内存空间（在堆中开辟内存空间、存放对象和对象属性）
+#pragma mark - 单个对象的内存管理
+/**
+ 6.内存中有5块内存空间
+ 栈 - 由编译器自动分配释放内存/主要存放函数的参数值、局部变量
+ 堆 - 由程序员手动分配释放内存/若程序员不释放，程序结束的时候才会回收
+ 静态区 -
+ 常量区 -
+ 程序代码区 - 程序加载到内存（程序代码存放在"代码区(存放二进制代码)"）
+ */
+/**
+ 7.内存管理有三种方式
+ 1.ARC/自动引用计数 - 不需要开发人员自己管理内存，编译器会在适当的地方加上 “release/retain代码”
+ 2.MRC/手动引用计数 - 所有对象的内存都需要开发人员手动管理/开发人员需要自己在适当的地方加上 “release/retain代码”
+ 3.自动释放池 - xxx
+ */
+/**
+ 8.内存管理的黄金法则
+ 1>.如果一个对象使用了 "alloc/copy[mutable]/retain" 那么就必须使用 "release/autorelease"
+ 2>.反之如果没有使用 "alloc/copy/retain" 就不能使用 "release/autorelease"
+ */
+-(void)singleObjectMemoryManager {
+    /**
+     1.开辟内存空间（在堆中开辟内存空间存放对象和对象属性）
      2.初始化所有属性
      3.返回指针地址（指针地址保存在栈区）
      */
+    /**
+    WM - 指针变量WM
+    WM指向的对象 - [[WMGameProxy alloc]init]
+    */
+    WMGameProxy *WM = [[WMGameProxy alloc]init];  // 引用计数器 = 1
+    // ARC/MRC混合编程 - 选中工程-> TARGET-> Build Settings-> Automatic Reference Counting-> NO
+    // 必须在 “{}” 前通过 “指针变量WM” 给 “WM指向的对象” 发送一条 “release消息”
+    [WM release];  // 引用计数器 - 1 = 0 -> 自动调用 "dealloc方法"/release只会使“引用计数器 - 1”不会“自动调用 'dealloc方法'”
+    
+//    WMGameProxy *WM = [[WMGameProxy alloc]init];  // 引用计数器 = 1
+//    [WM retain]; // 引用计数器 = 2
+//    [WM release]; // // 引用计数器 - 1 = 1/这时候不会“自动调用 'dealloc方法'”
+    
+    /**
+     0>.僵尸对象 - 被释放的对象/开启监听"僵尸对象"( Edit Scheme->Run->Diagnostics->勾选Zombie Objects )
+     1>.野指针 - 指向僵尸对象的指针
+     */
+    WMGameProxy *currentWM = [[WMGameProxy alloc]init]; // 引用计数器 = 1
+    [currentWM release]; // 引用计数器 - 1 = 0 -> 对象被释放/此时 [[WMGameProxy alloc]init] 为僵尸对象/ currentWM 为野指针
+    // 为了避免给野指针发送消息会报错，一般情况下当一个对象被释放后我们会将这个对象的指针设置为空指针
+    // 因为在OC中给空指针发送消息程序不会报错
+    currentWM = nil;
+    // 指针变量currentWM 调用 release方法  <==>  给 指针变量currentWM 发送 release消息
+    [currentWM release]; // 给 currentWM（野指针） 发送消息程序会报错
+    [currentWM release];
+    [currentWM release];
+}
+
+
+
+
+
+
+
+
+-(void)memoryManager {
     // 创建对象的时候返回的地址其实就是类的第0个属性的地址
     // 类的第0个属性是一个叫 isa 的属性
     // 其实类也是对象（类对象存放着当前类的所有方法，由系统自动创建）
@@ -43,25 +125,10 @@
      类方法：类对象 -> 调用方法
      类对象的执行效率更高
      */
-//    WMGameProxy *proxy = [[WMGameProxy alloc]init];
-//    proxy.mheight = 100;
-//    /// 基本对象的内存管理
-//    MainController *controller = [[MainController alloc]init];  // 引用计数为1
-//    NSLog(@"1.引用计数：%ld", [controller retainCount]);
-//    [controller retain];  // 引用计数+1
-//    NSLog(@"2.引用计数：%ld", [controller retainCount]);
-//    [controller retain];  // 引用计数+1
-//    NSLog(@"3.引用计数：%ld", [controller retainCount]);
-//    [controller release]; // 引用计数-1
-//    NSLog(@"4.引用计数：%ld", [controller retainCount]);
-//    [controller release]; // 引用计数-1
-//    [controller release]; // 引用计数-1（自动调用dealloc方法）
     /// 多个对象的持有某一个对象
     // 当你不再使用某一个对象引用计数-1
     // 对象所有权：当一个所有者（Objective-C对象）做出alloc/retain/copy操作就会拥有该对象的所有权
     // 释放对象所有权：做出 “release/autorelease操作” 就会释放该对象的所有权
-///     内存管理的黄金法则：如果对一个对象使用了 alloc/copy[mutable]/retain 那么必须使用 release/autorelease 释放
-///     ！！！反之如果没有使用 alloc/copy/retain 就不能使用 release/autorelease！！！
     // 怎么持有对象？？？
     // 对象销毁相当于野指针：调用野指针会 crash
     /*
--- WMGameProxy/WMGameProxy/WMThreadViewController.m
+++ WMGameProxy/WMGameProxy/WMThreadViewController.m
@@ -15,12 +15,6 @@
 @end
 
 /**
- 0.线程安全
- 1>.资源共享 - 多个线程访问同一块资源（用一个对象/同一个变量/同一个文件）
- 2>.多个线程资源共享很容易引发数据错乱和数据安全问题
- 3>.互斥锁 - 必须全局唯一/优点 - 能有效防止因多线程抢夺资源造成的数据安全问题/缺点 - 需要消耗大量的CPU资源
- */
-/**
  1.线程间通信
  1>.概念 - 在一个进程中，线程往往不是孤立存在的，多个线程之间需要经常进行通信
  2>.1).一个线程传递数据给另一个线程/2).一个线程执行完毕特定任务后通知另一个线程继续执行任务
@@ -34,6 +28,7 @@
 }
 
 // 1.NSThread
+// 1>.基本使用
 // 线程的生命周期 - 当线程中的任务执行完毕以后才会被释放
 -(void)setupNSThread {
     // 1>.创建线程/第一种方式
@@ -89,6 +84,12 @@
     NSLog(@"输出当前方法名%s", __FUNCTION__);
 }
 -(void)run:(NSString *)param {
+    /**
+    2.线程安全
+    1>.资源共享 - 多个线程访问同一块资源（用一个对象/同一个变量/同一个文件）
+    2>.多个线程资源共享很容易引发数据错乱和数据安全问题
+    3>.互斥锁 - 必须全局唯一/优点 - 能有效防止因多线程抢夺资源造成的数据安全问题/缺点 - 需要消耗大量的CPU资源
+    */
     // 互斥锁 - 必须全局唯一
     // 这里使用 self（锁对象） 保证全局唯一
     // 1.注意加互斥锁的位置
@@ -111,8 +112,7 @@
         }
     }
 }
-
-// 2.线程通信
+// 2>.线程通信
 -(void)setupDownload {
     [NSThread detachNewThreadSelector:@selector(download) toTarget:self withObject:nil];
 }
@@ -149,6 +149,228 @@
 -(void)reloadImageView:(UIImage *)image {
     // 5.显示UI/不能放在子线程修饰
     self.imageView.image = image;
+}
+
+
+// 2.GCD - Grand Central Dispatch/强大的中枢调度器
+//http://www.cocoachina.com/ios/20161031/17887.html
+//https://blog.csdn.net/u011146511/article/details/79300015
+//https://www.jianshu.com/p/96032a032c7c
+// 1>.基本使用
+// GCD纯C语言，提供了非常多强大的函数
+// GCD是苹果公司为多核的并行运算提出的解决方案
+// GCD会自动利用更多的CPU内核（比如双核、四核）
+// GCD会自动管理线程的生命周期（创建线程、调度线程、销毁线程）
+// 开发人员只需要告诉GCD想要执行什么任务/不需要编写任何线程管理代码
+/**
+ GCD中两个核心概念
+ 1.任务 - 执行什么操作
+ 2.队列 - 用来存放任务
+ */
+/**
+ GCD的执行步骤
+ 1.定制任务
+ 2.将任务添加到队列 - GCD会自动将队列中的任务取出，放在对应的线程中执行/任务的取出遵循队列的FIFO原则
+ */
+-(void)setupGCD {
+    // 1.GCD用来执行任务的常用函数
+    /**
+     1.queue - 队列
+     // 3>.并发队列
+     // 可以将多个任务并发（同时）执行/自动开启多个线程同时执行任务
+     // 并发功能只有在异步函数在才有效（因为同步函数不支持开启多线程）
+     // 4>.串行队列
+     // 让任务一个接着一个的按顺序执行/一个任务执行完毕在执行下一个任务
+     // 并发功能只有在异步函数在才有效（因为同步函数不支持开启多线程）
+     2.block - 任务
+     // 1>.用同步的方式来执行任务
+     // 同步 - 只能在当前线程中执行任务，不具备开启新线程的能力
+     // 2>.用异步的方式来执行任务
+     // 异步 - 可以在新的线程中执行任务，具备开启新线程的能力
+     */
+    // 1.异步 + 并发/会开启多条线程（具体开启多少条线程是由系统内部决定的/不受我们控制）/队列并发执行任务
+    // 1>.创建队列
+    /**
+     第一种方式
+     第一个参数 - C语言的字符串/标识符
+     第二个参数 - 队列类型/ DISPATCH_QUEUE_SERIAL串行队列/ DISPATCH_QUEUE_CONCURRENT并行队列
+     */
+    dispatch_queue_t ConcurrentQueue = dispatch_queue_create("com.easy.download", DISPATCH_QUEUE_CONCURRENT);
+    /**
+     第二种方式 - 获取全局并发队列
+     第一个参数 - 优先级
+     DISPATCH_QUEUE_PRIORITY_HIGH 2
+     DISPATCH_QUEUE_PRIORITY_DEFAULT  0
+     DISPATCH_QUEUE_PRIORITY_LOW (-2)
+     DISPATCH_QUEUE_PRIORITY_BACKGROUND INT16_MIN
+     第二个参数 - 为未来使用
+     */
+    // ConcurrentQueue == mainQueue等价的
+    dispatch_queue_t mainConcurrentQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    // 2.封装任务 -> 添加任务到队列中
+    /**
+    第一个参数 - 队列
+    第二个参数 - 需要执行的任务
+    */
+    // 一个队列中添加多个任务
+    dispatch_async(ConcurrentQueue, ^{
+        NSLog(@"download---%@", [NSThread currentThread]);
+    });
+    dispatch_async(ConcurrentQueue, ^{
+        NSLog(@"download---%@", [NSThread currentThread]);
+    });
+    dispatch_async(ConcurrentQueue, ^{
+        NSLog(@"download---%@", [NSThread currentThread]);
+    });
+    
+    // 2.异步 + 串行/会开启一条线程/队列串行执行任务
+    // 1>.创建队列
+    /**
+     第一个参数 - C语言的字符串/标识符
+     第二个参数 - 队列类型/ DISPATCH_QUEUE_SERIAL串行队列/ DISPATCH_QUEUE_CONCURRENT并行队列
+     */
+    dispatch_queue_t SerialQueue = dispatch_queue_create("com.easy.download", DISPATCH_QUEUE_SERIAL);
+//    dispatch_queue_t SerialQueue = dispatch_queue_create("com.easy.download", NULL);
+    /**
+     获取主队列 - 跟主线程相关联的串行队列
+     1.主队列是GCD自带的一种特殊的串行队列
+     2.放在主队列的任务必须在主线程中执行
+     */
+    /**
+     串行队列 - 在当前线程执行任务/有任务就必须执行（不会检查主线程的状态（是否在忙）都会强制让当前线程来执行任务）
+     主队列 - 如果主队列发现当前主线程有任务在执行那么主队列就会暂停调用队列中的任务直到主线程空闲为止
+     */
+    dispatch_queue_t mainQueue = dispatch_get_main_queue();
+    // 2.封装任务 -> 添加任务到队列中
+    /**
+    第一个参数 - 队列
+    第二个参数 - 需要执行的任务
+    */
+    // 一个队列中添加多个任务
+    dispatch_async(SerialQueue, ^{
+        NSLog(@"download---%@", [NSThread currentThread]);
+    });
+    dispatch_async(SerialQueue, ^{
+        NSLog(@"download---%@", [NSThread currentThread]);
+    });
+    dispatch_async(SerialQueue, ^{
+        NSLog(@"download---%@", [NSThread currentThread]);
+    });
+    
+    // 3.同步 + 并发/不会开启子线程/队列串行执行任务
+    // 1>.创建队列
+    // 2.封装任务 -> 添加任务到队列中
+    /**
+    第一个参数 - 队列
+    第二个参数 - 需要执行的任务
+    */
+    // 一个队列中添加多个任务
+    dispatch_sync(mainConcurrentQueue, ^{
+        NSLog(@"download---%@", [NSThread currentThread]);
+    });
+    dispatch_sync(mainConcurrentQueue, ^{
+        NSLog(@"download---%@", [NSThread currentThread]);
+    });
+    dispatch_sync(mainConcurrentQueue, ^{
+        NSLog(@"download---%@", [NSThread currentThread]);
+    });
+    
+    // 4.同步 + 串行/不会开启子线程/队列串行执行任务
+    // 1>.创建队列
+    // 2.封装任务 -> 添加任务到队列中
+    /**
+    第一个参数 - 队列
+    第二个参数 - 需要执行的任务
+    */
+    // 一个队列中添加多个任务
+    dispatch_sync(SerialQueue, ^{
+        NSLog(@"download---%@", [NSThread currentThread]);
+    });
+    dispatch_sync(SerialQueue, ^{
+        NSLog(@"download---%@", [NSThread currentThread]);
+    });
+    dispatch_sync(SerialQueue, ^{
+        NSLog(@"download---%@", [NSThread currentThread]);
+    });
+    
+    // 5.异步 + 主队列/所有任务都在主线程中执行，不会开启线程
+    // 异步 - 如果当前没有执行完毕，后面也可以执行
+    dispatch_async(mainQueue, ^{
+        NSLog(@"download---%@", [NSThread currentThread]);
+    });
+    dispatch_async(mainQueue, ^{
+        NSLog(@"download---%@", [NSThread currentThread]);
+    });
+    dispatch_async(mainQueue, ^{
+        NSLog(@"download---%@", [NSThread currentThread]);
+    });
+    
+    // 6.同步 + 主队列/死锁（相互等待）
+    // 如果把该段代码放在子线程就不会发生死锁
+    // 同步函数 - 立刻马上执行/如果我没有执行完毕后面的也别想执行
+    dispatch_sync(mainQueue, ^{
+        NSLog(@"download---%@", [NSThread currentThread]);
+    });
+    dispatch_sync(mainQueue, ^{
+        NSLog(@"download---%@", [NSThread currentThread]);
+    });
+    dispatch_sync(mainQueue, ^{
+        NSLog(@"download---%@", [NSThread currentThread]);
+    });
+}
+// 2>.线程通信
+-(void)notificationGCD {
+    // 1.创建子线程下载图片
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        // 1>.确定URL
+        NSURL *url = [NSURL URLWithString:@"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1584524053542&di=11684008fd1275a02127eb8e878ff887&imgtype=0&src=http%3A%2F%2Fa3.att.hudong.com%2F68%2F61%2F300000839764127060614318218_950.jpg"];
+        // 2>.根据url下载图片/能下载下来的是二进制数据
+        NSData *imageData = [NSData dataWithContentsOfURL:url];
+        // 3.转换图片格式
+        UIImage *image = [UIImage imageWithData:imageData];
+//        // 5.显示UI/不能放在子线程修饰
+//        self.imageView.image = image;
+        // 4.回到主线程
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.imageView.image = image;
+        });
+//        // 这里不会发生死锁
+//        dispatch_sync(dispatch_get_main_queue(), ^{
+//            self.imageView.image = image;
+//        });
+    });
+}
+// 3.GCD其他常见函数
+-(void)notify {
+    // 1.延迟执行
+    // 第一种方法
+    [self performSelector:@selector(onTask) withObject:nil afterDelay:2.0];
+    // 第二种方法
+    [NSTimer scheduledTimerWithTimeInterval:2.0 target:self selector:@selector(onTask) userInfo:nil repeats:YES];
+    // 第三种方法
+    /**
+     第一个参数 - 从什么开始计算时间
+     第二个参数 - 需要延迟的时间
+     第三个参数 - 队列
+     第四个参数 - 任务
+     */
+//    // 设置 “任务” 在主线程中执行
+//    dispatch_queue_t main = dispatch_get_main_queue();
+    // 设置 “任务” 在子线程中执行
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), queue, ^{
+        NSLog(@"GCD===%@", [NSThread currentThread]);
+    });
+    
+    // 2.一次性代码
+    // 整个应用程序运行期间只会执行一次
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        NSLog(@"----once----");
+    });
+}
+-(void)onTask {
+    NSLog(@"task===%@", [NSThread currentThread]);
 }
 
 @end
