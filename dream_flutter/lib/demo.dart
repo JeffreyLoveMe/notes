001094773ab48839e2e8c78e780081f20ff8ef15
xiewujun <xiewujun@thedream.cc>
2020-05-21 19:25:36 +0800

    note上传

--- dream_flutter/lib/demo.dart
+++ dream_flutter/lib/demo.dart
@@ -1,4 +1,15 @@
 // https://www.yiibai.com/dart/dart_programming_data_types.html
+/*
+文件导入
+1>.import 'xxx' - 全部导入
+2>.import 'xxx' show yyy - 部分导入 - 只导入yyy
+3>.import 'xxx' hide yyy - 部分导入 - 除yyy导入其他
+4>.import 'xxx' deferred as yyy - 延迟导入yyy
+ */
+import 'dart:collection';
+import 'package:xml/xml.dart' as xml;
+
+// shift + command + F --> 全局搜索
 class Demo {
   // 标识符 - 与其他语言一致
   // 以“字母/下划线”开头，只能包含“字母/下划线/数字/$”
@@ -26,6 +37,7 @@ class Demo {
     num sum = 0.01;  // num - 表示int/double的父类
     // int s = 0.1;  // 不要把小数分配给整数（会抛出异常）
     print(sum);
+
     // 二、字符串 - 可以使用''/""
     // dart语言中字符串是不可变的
     String c = '这是一个字符串';
@@ -55,6 +67,7 @@ class Demo {
     China
     ''';
     print(l);
+
     // 三、布尔类型 - 没有非零既真/必须有明确的真假
     assert(k.isEmpty); // 检查是否是空字符串
     assert(e == 0); // 检查零值
@@ -66,13 +79,15 @@ class Demo {
     } else {
       print('走到这里');
     }
+
     // 四、数组List - 有序
-    // 1>.固定长度列表 - 长度不能在运行时更改
+    // var goodList = <String>[];  // 推荐使用
+    // 1>.固定长度列表 - 默认已经指定长度/长度不能在运行时更改
     var listName = new List(3);
     listName[0] = 1;
     listName[1] = 2;
     listName[2] = 3;
-    // 2>.可增长列表 - 长度能在运行时更改
+    // 2>.可增长列表 - 默认没有指定长度/长度能在运行时更改
     // 创建一个空List
     var listM = new List();  
     listM.add(1);
@@ -92,6 +107,7 @@ class Demo {
       firstList.reversed;  // List逆序
       firstList.length;    // List长度
     }
+
     // 五、集合Set - 无序
     // 集合元素不能相同？？？
     var set = {'123', '1234', '1235'}; // 创建非空Set不需要指定类型
@@ -99,7 +115,10 @@ class Demo {
     set.length; // 获取集合长度
     emptySet.add('12345'); // 添加元素
     set.addAll(emptySet);  // 将emptySet中的元素添加到set中（不是把emptySet添加到set中）
+
+
     // 六、映射 - Map对象
+    // var goodMap = <String, Object>{};  // 推荐使用
     // dart语言中规定 - 映射中的键值对都可以是任何类型
     // 映射可以在运行时增长和缩小
     // 1>.第一种创建方式
@@ -114,7 +133,7 @@ class Demo {
     assert(gifts['key4'] == null);
     gifts.length;  // 返回映射中键值对的个数
     // 2>.第二种创建方式
-    // 创建赋值
+    // 创建赋值 - 不推荐使用
     var giftMaps = Map();
     giftMaps['key1'] = 'value1';
     giftMaps['key2'] = 'value2';
@@ -133,8 +152,10 @@ class Demo {
       3: 'value3'
     };
     print(map);
+
     // 七、符文 - UTF-32的代码点
     String.fromCharCode(1);
+
     // 八、符号Symbol - Symbol对象表示dart程序中声明的运算符或标识符
     // dart的符号是不透明的动态字符串的名称
     // 符号是一种存储人类可读字符串与优化供计算机使用的字符串之间关系的方法
@@ -143,18 +164,35 @@ class Demo {
   }
 
   // 变量 - 使用变量之前必须先声明变量
+  // 变量存储对值的引用/dart语言支持类型检查
+  // 注意 - dart语言中一切皆对象/未初始化的变量默认为null/数字类型、布尔类型没有初始化都默认为null
   /*
-  String - 
-  var - 
-  dynamic - 
+   * 问题 - var/Object/dynamic都可以声明变量，它们的区别是什么？
+   * var - 如果没有初始化值可以变成任何类型/如果有初始化值声明的变量类型固定不能改变
+   * Object - 动态任意类型/声明的变量类型可以改变/编辑的时候会检查类型
+   * dynamic - 动态任意类型/声明的变量类型可以改变/编译的时候不检查类型
    */
   void disVariable() {
-    // 变量存储对值的引用/dart语言支持类型检查
-    // 注意 - dart语言中一切皆对象/未初始化的变量默认为null/数字类型、布尔类型没有初始化都默认为null
-    String age = '18';
+    // 如果有初始化值声明的变量类型固定不能改变
+    // 支持类型推导
+    // 推荐使用
     var name = 'wy';
-    dynamic height = '180';
-    print('$name 今年 $age 岁，身高 $height');
+
+    // 动态任意类型/声明的变量类型可以改变/编译的时候会检查类型
+    // 不推荐使用
+    Object weight = '125';
+    // weight = 123;   // 编译的时候会报错
+
+    // 动态任意类型/声明的变量类型可以改变/编译的时候不检查类型
+    // 类似oc种的id类型
+    dynamic height = '180'; 
+    // height = 170; // 编译不报错/运行报错 - 编译的时候不检查类型
+
+    // 使用默认类型
+    // 类似java
+    String age = '18';
+
+    print('$name 今年 $age 岁，体重 $weight 身高 $height');
   }
 
   // 常量 - 不可以修改/final、const关键字用于声明常量
@@ -184,7 +222,16 @@ class Demo {
     result = (m1 | m2);
     result = (m1 ^ m2);
     result = (~m1);
-    print('$result');
+    result = (m1 ~/ m2); // 取整 - 实际上就是商
+    int sum = 2;
+    String s = sum as String; // as表示’类型转换‘
+    assert(sum is int);  // is代表‘是否是’
+    // ? - 可选类型
+    // ?? - 与swift一致
+    String msg;
+    print(msg?.length); 
+    print('$result/$s');
+
     // 2>.条件语句
     if (m1 > m2) {
       
@@ -221,6 +268,7 @@ class Demo {
   }
 
   // 函数 - 可读/可维护/可重用代码的代码块
+  // Function - 函数也是对象/函数也是对象/函数也是对象
   // 1>.普通函数
   void disNormal(int a, int b, int c) {
     int result = a + b + c;
@@ -259,14 +307,256 @@ class Demo {
       }
         break;
     }
+
+    // 时间处理
+    // 1>.获取当前时间
+    DateTime.now();
+    // 2>.创建一个指定时间年月日的DateTime对象
+    DateTime(2009, 03, 03);
+    // 3>.解析日期字符串
+    DateTime.parse('2019-03-02');
+    // 4.解析时间戳
+    DateTime.fromMillisecondsSinceEpoch(15516161200000);
   }
+
+  // dart集合
+  void disQueue() {
+    Queue queue = new Queue();
+    queue.addAll([12, 13, 14]);
+    // 遍历
+    Iterator iterator = queue.iterator;
+    while (iterator.moveNext()) {
+      print(iterator.current);
+    }
+  }
+
+  // 包 - 封装一组编程单元的一种机制
+  // App可能需要集成某些第三方库或插件 - 每种语言都会提供一种机制来管理外部软件包
+  /*
+  dart的软件包管理器是pub
+  1.pub get - 获取App所依赖的所有包
+  2.pub upgrade - 将所有依赖项升级到较新版本
+  3.pub build - 用于构建您的Web应用程序
+  4.pub help - 将提供所有pub命名帮助
+   */
+  // 下载第三方库：先在pubspec.yaml中写上需要下载的第三方库 -> 然后选中pubspec.yaml -> 最后Get Packages
+  void disXml() {
+    var bookXml = '''
+    <?xml version = "1.0"?> 
+      <bookshelf> 
+        <book> 
+          <title lang = "english">Growing a Language</title> 
+          <price>29.99</price> 
+        </book> 
+
+        <book> 
+         <title lang = "english">Learning XML</title> 
+         <price>39.95</price> 
+        </book> 
+      <price>132.00</price> 
+    </bookshelf>
+    ''';
+    var documentXml = xml.parse(bookXml);
+    print(documentXml.toString());
+  }
+
+  // 3.异常处理 - 在执行程序期间出现问题
+  // 特点 - 发生异常时程序的正常流程中断，程序会异常中止
+  void disException() {
+    // 第一种写法
+    try {
+      // 可能会导致异常的代码
+      testAge(-2);
+    } catch (e) {
+      // 如果发生异常在这里处理
+      CustomException exception = e;
+      exception.msgError();
+    } finally {
+      // 可选 - 总是会执行这里
+    }
+    // 第二种写法
+    try {
+      // 可能会导致异常的代码
+      testAge(-2);
+    } on Exception {
+      // 如果发生异常在这里处理
+    } finally {
+      // 可选 - 总是会执行这里
+    }
+  }
+  // 2.抛出异常
+  void testAge(int age) {
+    if (age < 0) {
+      // throw new FormatException();
+      throw new CustomException();
+    }
+  }
+  // 自定义异常 -> 抛出异常 -> 异常处理
+
+  // dart调试 - 查找和修复错误的过程称为调试
 }
 
-// 枚举
+// 1.自定义异常
+class CustomException implements Exception {
+  String msgError() => 'Amount should be greater than zero';
+}
+
+// 枚举 - 特殊的类，通常用来表示相同类型的一组常量
+// 枚举不能被继承，不能创建实例
 enum enumName {
   normalLoginType,
   phoneLoginType,
   codeLoginType
 }
 
-// 类 - xxx
+// 类
+// dart是一门面向对象的语言 - 支持面向对象编程
+// 声明一个类/dart不支持多继承/支持多重继承
+// 继承 - 子类可以继承父类除构造函数以外的所有属性和函数
+// dart支持方法重写
+class Test extends Demo {
+  // 成员变量
+  String name = 'wy';
+  // 静态变量
+  static int age = 12;
+  // ‘_’表示私有
+  String _method;
+  void _disAdd() {
+    _method = 'wy';
+    print(_method);
+  }
+  // setter方法
+  set testName(String name) {
+    this.name = name;
+  }
+  // getter方法
+  String get testName {
+    return this.name;
+  }
+  // 构造函数
+  // 1>.普通构造函数
+  Test() {
+    print('');
+  }
+  // 2>.命名构造函数
+  Test.nameConst() {
+    // this - 表示引用类的当前实例
+    this.name = '';
+  }
+  // 使用这些方法
+  void disDemo() {
+    // 0>.super - 直接调用父类方法
+    super.disConstant();
+    // 1>.实例化Test
+    Test test = new Test();
+    test = Test();
+    test = new Test.nameConst();
+    // 2>.调用成员变量
+    test.name = 'xwj';
+    // 3>.调用对象函数
+    test.disDemo();
+    test._disAdd();
+    // 4.调用静态函数
+    Test.disTest();
+  }
+  // 静态函数 - 需要类名调用
+  static void disTest() {
+    // 状态 - 描述对象
+    // 行为 - 描述对象可以执行的操作
+    // 标识 - 将对象与一组类似对象区分开来的唯一值
+  }
+}
+
+// 抽象类和接口
+abstract class Massage {
+  void doMassage();
+}
+
+class FootMassage implements Massage {
+  @override
+  void doMassage() {
+    print('脚底按摩');
+  }
+}
+
+class BackMassage implements Massage {
+  @override
+  void doMassage() {
+    print('背部按摩');
+  }
+}
+
+
+// // 2.常量声明 - 使用final和const
+//   // 第一、final和const的共同点
+//   // 1>.常量声明可以省略常量类型
+//   final String m6 = 'ab';
+//   // // 2>.常量初始化以后不能再次赋值
+//   // m6 = '123'; // 报错
+//   final m7 = 'ab';
+//   const String m8 = 'ab';
+//   const m9 = 'ab';
+//   // // 2>.不能与var一起使用（跟swift一样：var代表变量）
+//   // final var m10 = 'ab'; // 报错
+//   // const var m10 = 'ab'; // 报错
+//   // 第二、final和const的区别
+//   // 1>.类级别常量使用static const
+//   // 2>.const可以使用其他const常量的值来初始化其值
+//   const width = 100;
+//   const height = 100;
+//   const square = width * height;
+//   print(square);
+//   // 3>.使用const赋值声明（const可以省略）
+//   // 第二个const可以省略
+//   var list1 = const [1, 2, 3];  // list1数组可以修改/list1[x]元素不能修改
+//   const list2 = const [1, 2, 3]; // list1数组不能修改/list1[x]元素不能修改
+//   final list3 = const [1, 2, 3]; // list1数组不能修改/list1[x]元素不能修改
+//   // 4>.可以更改非final/非const变量的值（即使曾经具有const值）
+//   // 5>.const导致的不可变性是可传递的（子类会继承）
+//   // 6>.相同的const常量不会在内存中重复传递
+//   // 7>.const需要是编译时常量
+//   print(identical(list2, list3)); // 判断两个对象是否一致/返回true - 表示常量指向同一块内存
+
+
+//  // 6>.集合Set
+//   Set oneSet = Set();
+//   oneSet.addAll([1, 2, 3, 4]);
+//   Set twoSet = Set();
+//   twoSet.addAll([1, 2, 5, 6]);
+//   // 补集 - 存在于set1中 & 不存在于set2中
+//   var difference = oneSet.difference(twoSet);
+//   print(difference);
+//   // 交集 - 存在于set1中 & 存在于set2中
+//   var intersection = oneSet.intersection(twoSet);
+//   print(intersection);
+//   // 并集 - 存在于set1中 || 存在于set2中
+//   var union = oneSet.union(twoSet);
+//   print(union);
+
+
+//  // 8>.匿名函数
+//   // (name) - 参数
+//   // print('$name') - 代码块
+//   var mName = (name) => print('$name'); // 有参匿名函数
+//   var sName = () => print(''); // 无参匿名函数
+//   // 一个函数做为另一个函数参数
+//   // String - 函数返回类型
+//   // func - 函数名称/自定义 - 形参
+//   // str - 函数入参
+//   void sLogger(String func(str)) {
+
+//   }
+//   sLogger(mName);
+//   void state(String func()) {
+
+//   }
+//   state(sName);
+//   // 9>.函数别名 - ？？？
+//   // 5.闭包 - 返回一个函数/函数也是一个对象
+//   Function makeAdd(int a, int b) {
+//     return (int y) => a + y;
+//   }
+//   // 接收一个函数
+//   var addFunc = makeAdd(10, 12);
+//   // 打印结果
+//   print(addFunc(12));
--- dream_flutter/lib/main.dart
+++ dream_flutter/lib/main.dart
@@ -1,11 +1,111 @@
+// 跨平台解决方案
+// 1>.Web/Hybrid - 基于web相关技术来实现界面和功能/cordova
+// 2>.jsCore - 通过JsCore桥接调用原生服务/React Native/Weex
+// 3>.Native - 将某个语言编译成二进制文件，生成动态库/打包成apk&iap/flutter
+
+// 选择flutter的原因
+// 1>.Web/Hybrid - UI性能差，功能性Api缺失
+// 2>.RN - UI性能一般，开发体验差
+// 3>.flutter - UI性能好，开发体验较好
+
+// 什么是flutter
+// 1>.概念 - flutter是google开发的移动UI框架。可以快速的在iOS/Android上构建高质量的原生用户界面。
+// 2>.特点 - flutter可以与现有的代码一起工作/flutter是完全免费、开源的
+// 3>.兼容 - iOS/Android/wp/web
+
+// flutter组成
+// 1>.Engine(C++) - Skia/Dart/Text
+// 2>.Framework - Dart
+
+// flutter环境搭建
+/*
+ * 11.flutter环境准备
+ * 1>.打开官网https://flutter.io
+ * 2>.点击Docs->选择MacOS
+ * 3>.下载flutterSDK（以后flutter升级直接可以用命令行）
+ * 4>.将下载完成的flutterSDK放在某个位置（开始配置环境变量）
+ * 1.vim ~/.bash_profile
+    // 环境变量
+    export PATH=/Applications/flutter/bin:$PATH
+    // 针对国内用户
+    export FLUTTER_STORAGE_BASE_URL=https://storage.flutter-io.cn
+    export PUB_HOSTED_URL=https://pub.flutter-io.cn
+ * 2.source ~/.bash_profile
+ * 3.flutter doctor #检测flutter环境
+ * 5>.在iOS上配置环境 - 下载Xcode/安装git/安装CocoaPods
+ * 6>.Android Studio - Preferences - Plugins(下载Dart/Flutter)
+ */
+// 注意点
+// 1>.shift + command + p - 打开控制器面板
+// 2>.运行flutter项目之前可以使用“flutter doctor”命令行检测一下
+// 3>.？？？怎么升级flutterSDK？？？
+// Using Flutter in China - https://flutter.dev/community/china
+// 4>.VSCode新建项目命令
+// flutter create -i swift -a kotlin xxx
+// 5>.将项目导入到AS
+// 将flutter项目导入AS中会引导AS安装flutter插件/Dart插件
+
+// flutter项目文件
+/*
+ * dream_flutter
+ * .dart_tool - 工具包
+ * .idea - 环境配置
+ * android - 安卓包
+ * build - xxx
+ * ios - iOS包
+ * lib - 主要工程目录
+ * test - 测试事例
+ * .gitignore - 忽略文件
+ * .metadata - 元信息
+ * .packages - 包信息
+ * donewflutter.iml - xxx
+ * pubspec.lock - xxx
+ * pubspec.yaml - 当前项目的信息（很重要）
+ * README.md - xxx
+ */
+
+// dart语言简介
+// 1>.dart语言是一个由Google开发的通用编程语言。后来被Ecma（Ecma-408）批准为标准。用于构建web/服务器/桌面和移动应用
+// 2>.dart语言是一种面向对象的，类定义的，垃圾回收语言。使用C样式语法可以随意地转换成js
+// 3>.dart语言支持接口/mixin/抽象类/具体化泛型/静态类型
+
+// dart语言概念
+// 1>.dart语言中一切（无论变量、数字、函数、null）皆对象，所有对象都是Object类的子类
+// 2>.dart语言是强类型语言（一旦确定类型就不可以改变变量类型，编译的时候会进行语法检查）/Dart语言允许弱类型语言式的编程
+// 3>.dart语言在运行前解析（指定数据类型和编译时常量）可以提高运行速度
+// 4>.dart语言的统一入口是main函数
+// 5>.dart语言的私有特性是在变量名或者函数名前面加上"_"
+// 6>.dart支持async/await异步处理
+
+// dart语言类型系统
+// 1>.dart语言是类型安全的（dart语言使用静态类型检查和动态类型检查以确保变量的值总是与变量的静态类型匹配）
+// 2>.dart语言中类型声明是可选的（因为会在编译时执行类型推导）
+// 3>.num/double/int/String/List/Set/bool/Map/Object/dymamic/Function(/Symbol/runes)
+// 4>.dart语言中所有的类型都是对象（都继承自Object类）/在dart语言中null类型也继承自Object类
+
+// flutter详情介绍
+// 1>.dart语法编译 - dart是一种强类型、跨平台的客户端开发语言。具有专门为客户端优化、高生产力、快速高效、可移植易学的风格
+// 2>.flutter插件 - dart语言无法直接调用Android系统提供的Java接口，需要使用flutter插件来实现中转
+// 3>.Skia（siQ）图像处理引擎 - 2005年问世/用于Chrome浏览器/2007年被移植到Android平台
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
+// 新建一个类 - 类名首字母大写
+/*
+ * StatelessWidget - 死控件/不会随数据动态改变
+ * StatefulWidget - 活控件/会随数据动态改变
+ */
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // 万物皆Widget
+  // 选中 -> command -> 内部实现
+  /*
+  widget之间使用;
+  属性之间使用,
+  */
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
