fb56859eb9e399a29205020f82117eedce7fa4d3
xiewujun <xiewujun@thedream.cc>
2020-11-24 18:47:16 +0800

    note上传

--- GNGameProxy/src/GNGameProxy.java
+++ GNGameProxy/src/GNGameProxy.java
@@ -1,3 +1,5 @@
+import java.util.Scanner; // 导入包中的类
+
 public class GNGameProxy {
     /**
     * 计算机概述
@@ -138,7 +140,7 @@ public class GNGameProxy {
         System.out.println(0x100); // 十六进制 - 0x
 
         /**
-         * 原码补码反码
+         * 原码补码反码 - 参考iOS笔记
          */
 
         /**
@@ -205,17 +207,211 @@ public class GNGameProxy {
         System.out.println("5 + 5 = " + 5 + 5); // 5 + 5 = 55;
         System.out.println("5 + 5 = " + (5 + 5)); // 5 + 5 = 10;
 
+        // 字符char - 取值范围：0 ~ 65535
+        char a20 = 'a';
+        System.out.println(a20);
+        char a21 = 97;
+        System.out.println(a21);
+        // Java使用Unicode编码，char可以存放2个字节
+        // Java中的char可以存放中文：单个中文占2个字节
+        char a22 = '中';
+        System.out.println(a22);
 
+        // 运算符 - 对常量和变量进行操作的符号
+        // 1.算术运算符：+，-，*，/，%
+        System.out.println(10 / 3); // 输出3 - 整数相除结果只能是整数
+        System.out.println(10 / 3.0); // 输出3.33333 - 小数相除结果只能是小数（把其中一个数变成小数，另一个数会自动类型提升）
+        System.out.println(-2 / 5); // 输出-2 - 当左边的绝对值小于右边绝对值的时候结果为左边的值/结果只与左边的符号有关，与右边无关
+        // 2.自增自减运算符 - 自身永远都需要+1或-1
+        int a23 = 13;
+        a23 = a23++; // 13
+        a23 = ++a23; // 14
+        a23 = a23--; // 14
+        a23 = ++a23; // 15
+        /**
+         * 面试题 - 哪句话会报错
+         * byte b = 10;
+         * b++; // '++'是一个运算符所以肯定会有一个结果，底层会转化为'b = (byte)(b + 1);'
+         * b = b + 1; // 报错 - byte和int进行运算会自动类型提升为int，int再赋值给byte会报错
+         */
+        // 3.赋值运算符：+=，-=，*=，/=，=
+        int a24 = 3;
+        a24 += 4; // a24 = a24 + 4;
+        a24 -= 4; // a24 = a24 - 4;
+        a24 *= 4; // a24 = a24 * 4;
+        a24 /= 4; // a24 = a24 / 4;
+        /**
+         * 面试题 - 下面程序是否有问题？
+         * short s = 1; s = s + 1; // 报错 - short和int进行运算会自动类型提升为int，int再赋值给short会报错
+         * short s = 1; s += 1; // short s = (short)(s + 1);
+         */
+        // 4.关系运算符：<，<，>=，<=，==，!=，
+        System.out.println(4 != 3);
+        // 5.逻辑运算符：&（逻辑与），|（逻辑或），^（异或 - 相同为false，不同为true），!（逻辑非），&&（短路与），||（短路或）
+        int a25 = 15;
+        System.out.println(a15 > 10 & a15 < 20);
+        // 6.位运算符：操作二进制：补码参与运算
+        // &（位与-有0则0），｜（位或-有1则1），^（异或-相同则0，不同则1），～（按位取反）
+        System.out.println(6 & 3); // 2
+        System.out.println(6 | 3); // 7
+        System.out.println(6 ^ 3); // 5
+        System.out.println(~6); // -7（按位取反，包括高位/操作的是补码，保存的是原码（6 - 原码 - 补码 - 按位取反 - 原码））
+        System.out.println(5 ^ 10 ^ 10); // 5
+        /**
+         * 面试题：实现两个整数的交换
+         * int x = 10;
+         * int y = 5;
+         * // 1.引入第三方变量（实际开发推荐使用）
+         * int temp = x;
+         * x = y;
+         * y = temp;
+         * // 2.不引入第三方变量（有弊端：有可能超出int的取值范围）
+         * x = x + y;
+         * y = x - y;
+         * x = x - y;
+         * // 3.不引入第三方变量
+         * x = x ^ y;
+         * y = x ^ y;
+         * x = x ^ y;
+         */
+        // <<左移（左边最高位丢弃，右边补0），>>右移（最高位是0，左边补0；最高位是1，左边补1），>>>无符号右移（无论最高位是0或1，左边补0）
+        System.out.println(12 << 1); // 24（向左移动几位就是乘以2的几次幂）
+        System.out.println(12 << 2); // 48
+        System.out.println(12 >> 1); // 6（向右移动几位就是除以2的几次幂）
+        System.out.println(12 >> 2); // 3
+        // 7.三目运算符
+        int a26 = 10;
+        int a27 = 5;
+        int max = (a26 > a27) ? a26 : a27;
 
+        // 键盘输入
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入第一个整数：");
+        int x = scanner.nextInt();
+        System.out.println("请输入第二个整数：");
+        int y = scanner.nextInt();
 
+        // if语句 - 判断区间使用
+        // 1.判断条件必须是boolean
+        // 2.如果是一条语言'{}'可以省略，如果是多条语句'{}'不能省略（建议永远不要省略）
+        // 3.一般来讲，有"{"就没有分号
+        if (x > 10) {
+            // 语句体1
+        }
+        if (y < 10) {
+            // 语句体1
+        } else {
+            // 语句体2
+        }
+        if (x > 10) {
+            // 语句体1
+        } else if (y < 10) {
+            // 语句体2
+        } else {
+            // 语句体3
+        }
+        /**
+         * 面试题 - 下面的代码是否有错误
+         * if (x > 10) int z = 10; // 报错 - "int z = 10;"为两条语句
+         */
 
+        // switch语句 - 判断固定值使用（枚举）/理论上比"if语句"效率高
+        // "表达式x + y"可以接收：基本数据类型（byte，short，char，int）/可以自动类型提升为int、枚举（JDK1.5）、字符串（JDK1.7）
+        // 1.int
+        switch (x + y) {
+            // case后面只能是常量（必须不一样），不能是变量
+            case 1: {
+                System.out.println("123");
+            }
+            break;
+            case 2: {
+                System.out.println("456");
+            }
+            break;
+            // case穿透
+            case 3:
+            case 4:
+            case 5: {
+                System.out.println("123456");
+            }
+            break;
+            // default可以省略（不建议），可以放在任何位置（放在最后可以省略break）
+            // 不管放在任何位置都只会最后执行
+            default: {
+                System.out.println("789");
+            }
+            break;
+        }
+        // 2.String
+        String gender = "男士";
+        switch (gender) {
+            case "男士": {
+                System.out.println("男士");
+            }
+            break;
+            case "女生": {
+                System.out.println("女生");
+            }
+            break;
+            default: {
+                System.out.println("太监");
+            }
+            break;
+        }
 
+        // 循环语句
+        // 1.while语句 - 条件为true开始循环，条件为false终止循环
+        int x1 = 100;
+        while (x1 < 10) {
+            // 循环体
+            x1--;
+        }
+        // 2.do...while循环 - 条件为true开始循环，条件为false终止循环
+        do {
+            // 循环体
+            x1--;
+        } while (x1 < 10);
+        // 3.forx循环
+        for (int i = 0; i < 100; i++) {
+            // 循环体 - i是局部变量
+        }
+        // 4.死循环
+//        while (true) {
+//
+//        }
+//        do {
+//
+//        } while (true);
+//        for (;;) {
+//
+//        }
+        // 5.循环嵌套
+        // 外循环控制行数
+        outer: // 标号：合法标识符即可
+        for (int i = 0; i < 4; i++) {
+            // 内循环控制列数
+            inner: // 合法标识符即可
+            for (int j = 0; j < 5; j++) {
+//                System.out.println("*"); // 输出自动换行
+                System.out.print("*"); // 输出不自动换行
+//                break; // 终止循环 - 只可以使用于"循环语句"和"switch语言"
+//                break inner; // 跳出内部循环（与break;效果一样）
+//                break outer; // 跳出外部循环
+//                continue; // 跳出本次循环，开始下次循环 - 只可以使用于"循环语句"
+//                return; // 结束方法（返回值）
+            }
+            // 转义字符
+            System.out.print("\n");
+        }
+        /**
+         * 面试题 - 下面的代码是否能够执行?
+         * System.out.println("hello world");
+         * // 可以执行，因为"https:"是一个标号，"//www.baidu.com"是一个注释
+         * https://www.baidu.com
+         * System.out.println("hello java");
+         */
 
-
-
-
-
-
+        System.out.println("-----这是分割线-----");
 
 
 
