81edea612dd19e1dcabb30b09027a08669cafb9f
xiewujun <xiewujun@thedream.cc>
2021-04-22 18:57:01 +0800

    note

--- README.md
+++ README.md
@@ -5,8 +5,6 @@ DMGameProxy - 安卓
 
 donew_index - 前端
 
-donew_interview - 计算机基础
-
 flutter_shop - flutter
 
 GNGameProxy - java后台
--- flutter_donew/android/app/src/main/kotlin/com/example/flutter_donew/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_donew
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
